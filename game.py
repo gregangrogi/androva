3577f54a3531d1f365710a4ac5d96068f353a711
gregangrogi <andrey.maslovskiy2006@gmail.com>
2020-02-18 15:14:22 +0300

    c5

--- game.py
+++ game.py
@@ -24,8 +24,12 @@ class meu ():
         self.stage = 0
         self.images = images
         self.sprite = []
-        self.colors= [(173, 199, 195), (139, 156, 158)]
-
+        self.colors= [(173, 199, 195), (139, 156, 158), (222, 184, 126)]
+    def tooch(self, x, y, x1, y1):
+        if pygame.mouse.get_pos()[0] > x and pygame.mouse.get_pos()[0] < x1 and pygame.mouse.get_pos()[1]>y and pygame.mouse.get_pos()[1] < y1 :
+            return True
+        else:
+            return False
     def imp(self):
         for x in range (0, len(self.images)):
             self.sprite.append(pygame.image.load(self.images[x]))
@@ -38,14 +42,12 @@ class meu ():
             sc.blit(text("новая игра", 50, (255,255,255)), (875, 500))
             sc.blit(text("настройки", 50, (255,255,255)), (880, 550))
             sc.blit(text("выйти", 50, (255,255,255)), (910, 600))
-            if pygame.mouse.get_pos()[0] > 860 and pygame.mouse.get_pos()[0] <1080:
-                if pygame.mouse.get_pos()[1]>500 and pygame.mouse.get_pos()[1] <540 and moused:
-                    self.stage = 1
-                if pygame.mouse.get_pos()[1]>550 and pygame.mouse.get_pos()[1] <590 and moused:
-                    self.stage = 2
-                if pygame.mouse.get_pos()[1]>600 and pygame.mouse.get_pos()[1] <640 and moused:
-                    self.stage = 3
-
+            if  self.tooch(860, 500, 1080, 540) and moused:
+                self.stage = 1
+            if  self.tooch(860, 550, 1080, 590) and moused:
+                self.stage = 2
+            if  self.tooch(860, 600, 1080, 640) and moused:
+                self.stage = 3
 
         if self.stage == 2:
             sc.blit(self.sprite[0],  (0, 0))
@@ -59,16 +61,22 @@ class meu ():
         if self.stage == 3:
             pygame.quit()
 
-
-
-
-
-        #if
-
+        if self.stage == 1:
+            pygame.draw.rect(sc, self.colors[1], (0, 0, 1920, 1080))
+            if self.tooch(650, 300, 850, 700):
+                pygame.draw.rect(sc, self.colors[2], (650, 300, 200, 400))
+            else:
+                pygame.draw.rect(sc, self.colors[0], (650, 300, 200, 400))
+            if self.tooch(1050, 300, 1250, 700):
+                pygame.draw.rect(sc, self.colors[2], (1050, 300, 200, 400))
+            else:
+                pygame.draw.rect(sc, self.colors[0], (1050, 300, 200, 400))
+            sc.blit(self.sprite[1],  (650, 300))
 
 #изображеня==================================================
 
-bg = [(".\\bg\\MENU BG.png")]
+bg = [(".\\bg\\MENU BG.png"), (".\\obj\\abr\\adr-choose.png")]
+
 
 #переменные==================================================
 
